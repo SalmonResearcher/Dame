b48ee83704e4e6e19830b021bde53e951d9e1ba6
桐也 貝田 <220026@jc-21.jp>
2024-06-21 15:10:19 +0900

    終わらなさそう

--- PlayerState.h
+++ PlayerState.h
@@ -6,11 +6,14 @@ class Player;
 class PlayerState{
 public:
     //変更されたときに発動
-    virtual void Enter(Player* player) = 0;
+    virtual void Enter(Player* _pPlayer) = 0;
     //更新
-    virtual void Update(Player* player) = 0;
+    virtual void Update(Player* _pPlayer) = 0;
+
+    virtual void HandleInput(Player* _pPlayer) = 0;
+
     //終わるときに発動（アニメーションなど）
-    virtual void Exit(Player* player) = 0;
+    virtual void Exit(Player* _pPlayer) = 0;
 
     //状態変化
     void Changestate(PlayerState* _nowState, PlayerState* _state);
